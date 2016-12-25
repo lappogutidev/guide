627af4e3d21bc4e2dfb0f673179182ababfe5d82
lappogutidev <lappoguti00@gmail.com>
2016-12-25 17:37:50 -0500

    minor changes to the string.js file

---
+++ string.js
@@ -0,0 +1,7 @@
+document.write("null character : \0<br />");
+document.write("single quote : \'<br />");
+document.write("double quote : \"<br />");
+document.write("backslash : \\<br />");
+
+var sampStr = "Random String";
+console.log("Returns the character at the index of 7 : ", sampStr.charAt(7))
